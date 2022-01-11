0955001c71da880e9ca4df31869ee02d541066b1
Jerod Houghtelling <houghtelling@gmail.com>
2022-01-10 19:21:13 -0700

    Removed lodash dependency

--- src/screens/SelectGroupsView.tsx
+++ src/screens/SelectGroupsView.tsx
@@ -6,7 +6,6 @@ import { Text, SafeAreaView, View, FlatList, Pressable } from "react-native";
 import { Group } from "../models/Group";
 import { StackParamList } from "../routes/MessageStack";
 import { Constants, globalStyles } from "../styles/Global";
-import _ from "lodash";
 import HeaderButton from "../components/HeaderButton";
 import Repository from "../utils/Repository";
 
@@ -30,7 +29,7 @@ const SelectGroupsView = ({ navigation, route }: SelectGroupsViewProps) => {
   const selectContacts = () => {
     navigation.navigate("SelectContacts", {
       message: route.params.message,
-      groups: _.filter(groups, "selected"),
+      groups: groups.filter(g => g.selected),
     });
   };
 
